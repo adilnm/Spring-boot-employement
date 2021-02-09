e2d4b8b23ea612f5961fe50d7178045f2ba22f22
Adil Naimi <adilnm23@gmail.com>
2021-02-08 23:31:36 -0600

    Edit controller

--- src/main/java/com/main/controller/EmployeeRestController.java
+++ src/main/java/com/main/controller/EmployeeRestController.java
@@ -3,8 +3,11 @@ package com.main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,18 @@ public class EmployeeRestController {
 	EmployeeDTO showEmployee(@PathVariable("id") int employeeId) {
 		return employeeService.show(employeeId);
 	}
+
+	@DeleteMapping("/employee/{id}")
+	String deleteEmployee(@PathVariable("id") int employeeId) {
+		employeeService.delete(employeeId);
+		return "Your employee got deleted";
+	}
+
+	@PostMapping("/employee")
+	void register(@RequestBody EmployeeDTO employee) {
+
+		employeeService.register(employee);
+
+	}
+
 }
